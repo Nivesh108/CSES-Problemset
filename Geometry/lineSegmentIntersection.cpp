a221a4bb0a5940e27d96b4595bcb5663dd7d1322
Nivesh108 <sharmanivesh08@gmail.com>
2022-05-19 23:51:10 +0530

    exponention

---
+++ Geometry/lineSegmentIntersection.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+#define ll long long 
+using namespace std;
+
+int main(){
+    ll x1,x2,x3,x4,y1,y2,y3,y4;
+    cin>>x1>>y1>>x2>>y2>>x3>>y3>>x4>>y4;
+    
+    return 0;
+}
--- Mathematics/exponentiation.cpp
+++ Mathematics/exponentiation.cpp
@@ -1,14 +1,21 @@
 #include<bits/stdc++.h>
-#define     ll          long long
+#define     ll          long long 
 #define     MOD         1000000007
 #define     t(t)        int t; cin>>t; while(t--) 
 #define     fast        ios::sync_with_stdio(0); cin.tie(0);
 using namespace std;
 
-ll power(ll a, ll b){
-    if(b==1) return a;
-    if(b%2==0)  return (pow(a,b/2)*pow(a,b/2));
-    else return a*pow(a,b/2)*pow(a,b/2);
+int m = 1e9+7;
+
+ll power(ll a,ll b){
+     ll res = 1;
+    while (b > 0) {
+        if (b & 1)
+            res = ((res%m) * (a%MOD))%MOD;
+        a = ((a%MOD) * (a%MOD));
+        b >>= 1;
+    }
+    return res;
 }
 
 int main(){
