b2ee4a8a9b6587a803e8adcd4ae0ed635fb615b0
Nivesh108 <sharmanivesh08@gmail.com>
2022-08-13 23:00:42 +0530

    Sum of Three Values

--- Sorting and Seaching/sumOfThreeValues.cpp
+++ Sorting and Seaching/sumOfThreeValues.cpp
@@ -4,12 +4,29 @@ using namespace std;
 
 int main(){
 
-    ll n,x;
-    cin>>n>>x;
-    vector<ll> v(n);
-    for(int i=0;i<n;++i) cin>>v[i];
+    ll n, sum;
+    cin >> n >> sum;
+    vector<pair<ll, ll>> v(n);
+    for(int i = 0; i < n; ++i){
+        cin >> v[i].first;
+        v[i].second = i + 1;
+    }
     sort(v.begin(),v.end());
-    int i=0,j=n-1,k=n/2;
-    // while()
+    
+    for(int i = 0; i < n - 2; i++){
+        int j = i + 1, k = n - 1;
+        int target = sum - v[i].first;
+        while(j < k){
+            if(v[j].first + v[k].first == target){
+                cout << v[i].second << ' ' << v[j].second << ' ' << v[k].second;
+                return 0;
+            }
+            else if(v[j].first + v[k].first > target) k--;
+            else j++;
+        }
+    }
+
+    cout << "IMPOSSIBLE";
+
     return 0;
 }
